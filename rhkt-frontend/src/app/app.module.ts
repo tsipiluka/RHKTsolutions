49ac5ab9b7214c2d4b3702c60b8325d00065f4b1
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2023-02-25 20:23:40 +0100

    profiles done

--- rhkt-frontend/src/app/app.module.ts
+++ rhkt-frontend/src/app/app.module.ts
@@ -11,10 +11,12 @@ import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
 import { DescribitionCardComponent } from './components/describition-card/describition-card.component';
 import { LogoCardComponent } from './components/logo-card/logo-card.component';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 
 @NgModule({
   declarations: [AppComponent, ProfileComponent, NavbarComponent, DescribitionCardComponent, LogoCardComponent],
-  imports: [BrowserModule, AppRoutingModule, CardModule, DividerModule],
+  imports: [BrowserModule, AppRoutingModule, CardModule, DividerModule, AvatarModule, AvatarGroupModule],
   providers: [],
   bootstrap: [AppComponent],
 })
